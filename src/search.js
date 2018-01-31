8963df7a10c9a214eba25395a1ca21ba77a9ac17
Owcollier <owcollier@gmail.com>
2018-01-31 15:19:04 -0500

    binary search fully implemented and  working correctly

--- src/search.js
+++ src/search.js
@@ -3,11 +3,13 @@ import React from 'react';
 export default class Search extends React.Component {
   constructor(props) {
     super(props);
+    this.searchCount = 0;
     this.state = {
       data: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
     }
 
     this.onClick = this.onClick.bind(this);
+    this.binarySearch = this.binarySearch.bind(this);
   }
 
   linearSearch(input) {
@@ -22,37 +24,57 @@ export default class Search extends React.Component {
    return `${input} not found. Search took ${searchCount} steps.`;
   }
 
-  binarySearch(array, input, start, end) {
-    let searchCount = 0;
-    if (start === undefined) {
-      start = 0;
+  binarySearch(array, input, start = 0, end = array.length) {
+    this.searchCount++;
+    // if (start === undefined) {
+    //   start = 0;
+    // }
+    // if (end === undefined) {
+    //   end = array.length;
+    // }
+    if (input > array[end - 1]) {
+      let output = `${input} not found. Search took ${this.searchCount} steps.`;
+      this.searchCount = 0;
+      console.log('base case');
+      return output;
     }
-    if (end === undefined) {
-      end = array.length;
+
+    if (start > end) {
+      let output = `${input} not found. Search took ${this.searchCount} steps.`;
+      this.searchCount = 0;
+      console.log('base case');
+      return output;
     }
 
     const index = Math.floor((start + end) / 2);
     const item = array[index];
 
     if (item === input) {
-      searchCount++;
-      return `${input} successfully found. Search took ${searchCount} steps.`;
+      let output = `${input} at index ${index} successfully found. Search took ${this.searchCount} steps.`;
+      this.searchCount = 0;
+      return output;
     }
     else if (item < input) {
-      searchCount++;
       return this.binarySearch(array, input, index + 1, end);
     }
     else if (item > input) {
-      searchCount++;
       return this.binarySearch(array, input, start, index - 1);
     }
-    else {
-      // console.log('input:', input);
-      // console.log('index:', index);
-      // console.log('item:', item);
-      return `${input} not found. Search took ${searchCount} steps.`;
-    }
+
+    this.searchCount = 0;
+    // else {
+    //   // console.log('input:', input);
+    //   // console.log('index:', index);
+    //   // console.log('item:', item);
+    //   return `${input} not found. Search took ${this.state.searchCount} steps.`;
+    // 
   };
+
+  incrementSearchCount() {
+    this.setState({
+      searchCount: this.state.searchCount + 1
+    })
+  }
 
 
   onClick(e) {
@@ -62,7 +84,8 @@ export default class Search extends React.Component {
     if (e.target.className === 'linear') {
       console.log(this.linearSearch(input));
     } else {
-      const data = this.state.data.sort();
+      const data = this.state.data.sort((a,b) => a-b);
+      console.log('HERE>>>>', data);
       console.log(this.binarySearch(data, input));
     }
     this.input.value = '';
